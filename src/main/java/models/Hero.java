7b8fa7e5e5699ac399fe6d02416caf17b7efb0cc
twstoly <twstoly@gmail.com>
2022-01-30 11:00:21 +0300

    add instance variables for Hero Class

---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,56 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Hero {
+    private String name;
+    private String age;
+    private String power;
+    private String weakness;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
