1ff16b328485d0429037c43a904aece90205b1eb
twstoly <twstoly@gmail.com>
2022-01-30 10:53:22 +0300

    add instance variables to our class Squad - squad name size task

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -2,7 +2,11 @@ package models;
 
 import java.util.ArrayList;
 public class Squad {
-
+    private String squad;
+    private String size;
+    private String task;
+    private static ArrayList<Squad> instances = new ArrayList<>();
+    private int id;
 
 
 
