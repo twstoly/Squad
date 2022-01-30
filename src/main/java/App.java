aa2f267d19467ac75e4da8b143504cfb74ef252d
twstoly <twstoly@gmail.com>
2022-01-30 10:45:08 +0300

    add get method for path homepage getting values for the static instances of Hero POJO static arrayList for instances.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,88 @@
+import models.Hero;
+import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+//        Showing all posts.
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero", hero);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
